f72fe848474e305bc04aff2426c7ed47cbf13c59
Dhananjay Goyal <113744303+DhananjayGoyalGL@users.noreply.github.com>
2023-12-05 21:53:44 +0530

    Time: 2 ms (83.01%) | Memory: 41.2 MB (5.39%) - LeetSync

---
+++ Leetcode/20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,40 @@
+class Solution {
+  public boolean isValid(String s) {
+    Stack<Character> stack = new Stack<>();
+
+    for (int i = 0; i < s.length(); i++) {
+      char ch = s.charAt(i);
+
+      switch (ch) {
+        case '(':
+        case '{':
+        case '[':
+          stack.push(ch);
+          break;
+        case ')':
+          if (stack.isEmpty() || stack.peek() != '(') {
+            return false;
+          }
+          stack.pop();
+          break;
+        case '}':
+          if (stack.isEmpty() || stack.peek() != '{') {
+            return false;
+          }
+          stack.pop();
+          break;
+        case ']':
+          if (stack.isEmpty() || stack.peek() != '[') {
+            return false;
+          }
+          stack.pop();
+          break;
+        default:
+          // Do nothing
+          break;
+      }
+    }
+
+    return stack.isEmpty();
+  }
+}
