3413c53b38a4a57cfca88651c6087f0aa23b28a5
Dhananjay Goyal <113744303+DhananjayGoyalGL@users.noreply.github.com>
2023-12-06 08:17:06 +0530

    Time: 1 ms (98.71%) | Memory: 40.2 MB (89.19%) - LeetSync

--- Leetcode/20-valid-parentheses/valid-parentheses.java
+++ Leetcode/20-valid-parentheses/valid-parentheses.java
@@ -2,39 +2,38 @@ class Solution {
   public boolean isValid(String s) {
     Stack<Character> stack = new Stack<>();
 
-    for (int i = 0; i < s.length(); i++) {
-      char ch = s.charAt(i);
+    for(int i=0; i<s.length(); i++){
+        char ch = s.charAt(i);
 
-      switch (ch) {
-        case '(':
-        case '{':
-        case '[':
-          stack.push(ch);
-          break;
-        case ')':
-          if (stack.isEmpty() || stack.peek() != '(') {
-            return false;
-          }
-          stack.pop();
-          break;
-        case '}':
-          if (stack.isEmpty() || stack.peek() != '{') {
-            return false;
-          }
-          stack.pop();
-          break;
-        case ']':
-          if (stack.isEmpty() || stack.peek() != '[') {
-            return false;
-          }
-          stack.pop();
-          break;
-        default:
-          // Do nothing
-          break;
-      }
-    }
+        switch(ch){
+            case '(':
+            case '{':
+            case '[':
+            stack.push(ch);
+            break;
 
-    return stack.isEmpty();
+            case')':
+            if(stack.isEmpty() || stack.peek() != '(') {
+                return false;
+            } stack.pop();
+            break;
+    
+            case'}':
+            if(stack.isEmpty() || stack.peek() != '{') {
+                return false;
+            } stack.pop();
+            break;
+    
+
+           case']':
+           if(stack.isEmpty() || stack.peek() != '[') {
+               return false;
+           } stack.pop();
+           break;
+    
+    case default: break;
+  }
+} 
+return stack.isEmpty();
   }
 }
